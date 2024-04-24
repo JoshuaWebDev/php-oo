3f3346969ec0893251b3acd142a2b8b488635c8b
Josué Barros da Silva <josue.barros1986@gmail.com>
2024-04-24 20:38:03 -0300

    Funcionario.php class refactored

--- classes/Funcionario.php
+++ classes/Funcionario.php
@@ -7,17 +7,59 @@ class Funcionario
     private $salario;
 
     /**
+     * Altera o valor de $codigo
+     * @param float $valor 
+     * @return void
+     */
+    public function setCodigo($valor)
+    {
+        $this->codigo = $valor;
+    }
+
+    /**
+     * Exibe o valor de $codigo
+     * @return float
+     */
+    public function getCodigo()
+    {
+        return $this->codigo;
+    }
+
+    /**
+     * Altera o valor de $dataDeNascimento
+     * @param float $valor 
+     * @return void
+     */
+    public function setDataDeNascimento($valor)
+    {
+        $this->dataDeNascimento = $valor;
+    }
+
+    /**
+     * Exibe o valor de $dataDeNascimento
+     * @return float
+     */
+    public function getDataDeNascimento()
+    {
+        return $this->dataDeNascimento;
+    }
+
+    /**
+     * Altera o valor de $salario
      * @param float $valor 
      * @return void
      */
     public function setSalario($valor)
     {
-        if (is_numeric($valor) and $valor > 0) {
+        if (is_numeric($valor) && $valor > 0) {
             $this->salario = $valor;
+        } else {
+            echo "<strong style='color:red'>Insira um valor numérico e positivo!</strong>";
         }
     }
 
     /**
+     * Exibe o valor de $salario
      * @return float
      */
     public function getSalario()
--- index.php
+++ index.php
@@ -4,6 +4,22 @@ require_once "classes/Funcionario.php";
 
 $pedro = new Funcionario();
 
-$pedro->setSalario(1412.5);
+$pedro->nome = "Pedro Pereira";
+echo "Nome do Funcionário: " . $pedro->nome . "<br>";
 
-echo "Salário: R$ " . number_format($pedro->getSalario(), '2', ',', '.');
+// o código abaixo apresenta erro porque o atributo codigo é privado
+// $pedro->codigo = '1234';
+// echo "Código do Funcionário: " . $pedro->codigo . "<br>";
+
+$pedro->setCodigo('45126');
+echo "Código do Funcionário: " . $pedro->getCodigo() . "<br>";
+
+$pedro->setDataDeNascimento('10/05/1989');
+echo "Data de Nascimento: " . $pedro->getDataDeNascimento() . "<br>";
+
+// o código abaixo apresenta erro porque o atributo salario é privado
+// $pedro->salario = 1412.5;
+// echo "Salário do Funcionário: " . $pedro->salario . "<br>";
+
+$pedro->setSalario(1412.5);
+echo "Salário do Funcionário: " . $pedro->getSalario() . "<br>";
